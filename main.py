4d84d7c8d7a1806e59a8ac84b491fdc35526d02b
zombfeed <rdgammad@gmail.com>
2025-12-22 13:19:55 -0800

    CH1:L4, check token metadata

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ def main():
         raise RuntimeError("API Key not found")
 
     client = genai.Client(api_key=api_key)
+    userprompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=userprompt)
+    metadata = response.usage_metadata
+    if not response:
+        raise RuntimeError("failed API request")
+    print(f"User prompt:{userprompt}")
+    print(f"Prompt tokens: {metadata.prompt_token_count}\nResponse tokens: {metadata.candidates_token_count}")
+    print(f"Response: {response.text}")
 
-    print(client.models.generate_content(model="gemini-2.5-flash", 
-                                         contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-          .text)
 
 if __name__ == "__main__":
     main()
