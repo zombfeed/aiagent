7258eb0565d87315305d4dbdbf78cd39d1e9f8fa
zombfeed <rdgammad@gmail.com>
2025-12-22 13:36:52 -0800

    CH1:L5, add user input, reorganize code

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
-
-def main():
+def get_api_key():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("API Key not found")
+    return api_key
 
-    client = genai.Client(api_key=api_key)
-    userprompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=userprompt)
-    metadata = response.usage_metadata
+def parse_user_prompt():
+    parser = argparse.ArgumentParser(description="AI Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User Prompt")
+    args = parser.parse_args()
+    return args.user_prompt
+
+def prompt_ai(client, prompt):
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
     if not response:
         raise RuntimeError("failed API request")
-    print(f"User prompt:{userprompt}")
+    metadata = response.usage_metadata
+    return response, metadata
+
+def main():
+    api_key = get_api_key()
+    client = genai.Client(api_key=api_key)
+    
+    userprompt = parse_user_prompt()
+    response, metadata = prompt_ai(client, userprompt)
+    
+    print(f"User prompt: {userprompt}")
     print(f"Prompt tokens: {metadata.prompt_token_count}\nResponse tokens: {metadata.candidates_token_count}")
     print(f"Response: {response.text}")
 
